a1f6cc82fdc8651b8f18e074384555511cfab4d7
Anahita Sabaghi <anahita.rastegar@gmail.com>
2024-09-24 17:24:27 +0330

    Built the Xylophone app following tutorial videos and completed the sharp corners challenge using RoundedRectangleBorder with BorderRadius.zero and Dart's Arrow Syntax!

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(XylophoneApp());
-}
+void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) => AudioPlayer().play(AssetSource('note$soundNumber.wav'));
 
-
-  void playSound(int soundNumber) {
-    final AudioPlayer player = AudioPlayer();
-    player.play(AssetSource('note$soundNumber.wav'));
-  }
-
-  Expanded buildKey({required Color color, required int soundNumber}) {
-    return Expanded(
-      child: TextButton(
-        style: TextButton.styleFrom(
-          backgroundColor: color,
+  Expanded buildKey({required Color color, required int soundNumber}) => Expanded(
+    child: TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
         ),
-        onPressed: () {
-          playSound(soundNumber);
-        },
-        child: Container(),
       ),
-    );
-  }
+      onPressed: () => playSound(soundNumber),
+      child: Container(),
+    ),
+  );
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
-              buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
-              buildKey(color: Colors.purple, soundNumber: 7),
-            ],
-          ),
+  Widget build(BuildContext context) => MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildKey(color: Colors.red, soundNumber: 1),
+            buildKey(color: Colors.orange, soundNumber: 2),
+            buildKey(color: Colors.yellow, soundNumber: 3),
+            buildKey(color: Colors.green, soundNumber: 4),
+            buildKey(color: Colors.teal, soundNumber: 5),
+            buildKey(color: Colors.blue, soundNumber: 6),
+            buildKey(color: Colors.purple, soundNumber: 7),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
 }
